2c54b302caa1e68a3a9144b7baf5d68c5f9142a3
MohammadArsalan <a4arshi@yahoo.com>
2021-10-30 12:13:16 +0500

    save data in async storage

--- src/contexts/QuizContext/QuizContext.jsx
+++ src/contexts/QuizContext/QuizContext.jsx
@@ -1,13 +1,26 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { Alert } from "react-native";
-import { TOTAL_LIVES } from "../../constants";
-import { checkIndexOutOfBound } from "../../utils/helpers";
+import { TOTAL_LIVES, LIVES_STORAGE_KEY, QUESTION_STORAGE_KEY } from "../../constants";
+import { checkIndexOutOfBound, saveAsyncStorage, getAsyncStorage } from "../../utils";
+
 import questions from "../../data/allQuestions";
+
 export const QuizContext = createContext(null);
 
 export const QuizProvider = ({ children }) => {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
-    const [ lives, setLives ] = useState(TOTAL_LIVES);
+    const [lives, setLives] = useState(TOTAL_LIVES);
+    const [hasLoaded, setHasLoaded] = useState(false);
+
+    useEffect(() => {
+        loadData();
+    }, []);
+
+    useEffect(() => {
+        if(hasLoaded) {
+            saveData()
+        }
+    }, [lives, currentQuestionIndex]);
 
     const onCorrect = () => {
         const nextIndex = checkIndexOutOfBound(questions, currentQuestionIndex + 1) ? 0 : currentQuestionIndex + 1;
@@ -29,6 +42,25 @@ export const QuizProvider = ({ children }) => {
         } else {
             setLives(lives - 1)
         }
+    }
+
+    const saveData = async () => {
+        await saveAsyncStorage(LIVES_STORAGE_KEY, lives.toString());
+        await saveAsyncStorage(QUESTION_STORAGE_KEY, currentQuestionIndex.toString());
+    }
+    const loadData = async () => {
+        const lives = await getAsyncStorage(LIVES_STORAGE_KEY);
+        if(lives) {
+            setLives(parseInt(lives));
+        }
+
+        const questionIndex = await getAsyncStorage(QUESTION_STORAGE_KEY);
+        if(questionIndex != null) {
+            setCurrentQuestionIndex(parseInt(questionIndex))
+        }
+
+        setHasLoaded(true)
+
     }
     return (
         <QuizContext.Provider
